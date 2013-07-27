d6727e761474b63784b55318c2d2ec4a5eaf72ee
RamakrishnanK <ramakrishnan@arrivusystems.com>
2013-07-27 15:42:05 -0400

    tld size changes

--- config/initializers/subdomain-fu.rb
+++ config/initializers/subdomain-fu.rb
@@ -2,7 +2,7 @@
 SubdomainFu.configure do |config|
   config.tld_sizes = {:development => 1,
                          :test => 0,
-                         :production => 1} 
+                         :production => 2} 
   config.mirrors = %w(www site we)
   config.preferred_mirror = "www"
 end
