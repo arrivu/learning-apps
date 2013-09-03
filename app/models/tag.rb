4545a1958a63c112246056455846876316d93c5c
samuel santhosh <samuel@arrivusystems.com>
2013-09-03 18:22:19 +0530

    add tokenizer tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -14,5 +14,9 @@ class Tag < ActiveRecord::Base
       tags
     end
   end
+  def self.ids_from_tokens(tokens)
+    tokens.gsub!(/<<<(.+?)>>>/) { create!(name: $1).id }
+    tokens.split(',')
+  end
 
 end
