96cfaf1cb8bf8217fc0d6fe1e362737eb3052ef5
franklinjagan <franklinjagan@arrivusystems.com>
2013-09-10 16:06:32 +0530

    course center

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -4,4 +4,9 @@ module TopicsHelper
       render(topic) + content_tag(:div, nested_topics(sub_topics), :class => "nested_topics")
     end.join.html_safe
   end
+    def nested_topic_index(topics)
+    topics.map do |topic, sub_topics|
+      render(topic_index) + content_tag(:div, nested_topics(sub_topics), :class => "accordion-inner")
+    end.join.html_safe
+  end
 end
