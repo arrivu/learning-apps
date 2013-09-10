fdf979b2906a0560d4d73cf3f3fd851896ecc812
franklinjagan <franklinjagan@arrivusystems.com>
2013-09-10 16:10:09 +0530

    courselist changes

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -4,9 +4,5 @@ module TopicsHelper
       render(topic) + content_tag(:div, nested_topics(sub_topics), :class => "nested_topics")
     end.join.html_safe
   end
-    def nested_topic_index(topics)
-    topics.map do |topic, sub_topics|
-      render(topic_index) + content_tag(:div, nested_topics(sub_topics), :class => "accordion-inner")
-    end.join.html_safe
-  end
+  
 end
