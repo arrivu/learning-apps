f9c08480301f8326a43ddc6dc174658dfab77fc1
samuel santhosh <samuel@arrivusystems.com>
2013-09-23 14:02:46 +0530

    category list uncomplte

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,10 +8,7 @@ class Ability
       can :manage, :all
       cannot :my_courses, Course
     elsif user.has_role? :teacher
-        can [:new,:create,:edit,:update, :destroy,:manage_courses,:course_status_search,
-             :completed_courses,:updatecompleted_details,:conclude_course,:concluded_course_update], Course
-        
-        can [:show_image,:background_image,:index,:show,:my_courses,:review],Course
+        can :manage, Course
         can [:show_image_detail,:theme_image_detail,:show_image_show,:theme_image_show],HeaderDetail
         can :manage, CoursePricing
         can :manage ,CoursePreview
@@ -19,17 +16,13 @@ class Ability
         can :manage,TeachingStaff
 
     else
-
-      can [:show_image,:background_image,:index,:show,:review],Course
-
-
-        can :manage,TeachingStaff
-
+      can [:show_image,:background_image,:index,:show,:tagged_courses,:get_modules,:datewise_courses,:popular_courses,:upcomming_courses,:subscribed_courses],Course
+      can :manage,TeachingStaff
       can [:show_image_detail,:theme_image_detail,:show_image_show,:theme_image_show],HeaderDetail
       can [:teaching_staff_signup] ,TeachingStaff
       can [:account_subscription] ,Account
       can :terms ,TermsAndCondition
-
+      can :show ,Topic
     end
 
   end
