965e9e14e5a60c94eb883136a577e6773e32e3f5
saravananNV <saranjasminenv@gmail.com>
2013-10-07 11:04:16 +0530

    Added_newrelic_gem

--- db/schema.rb
+++ db/schema.rb
@@ -317,7 +317,7 @@ ActiveRecord::Schema.define(:version => 20130314093035551) do
     t.integer  "item"
     t.string   "table"
     t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 5
+    t.integer  "year",       :limit => 8
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
   end
