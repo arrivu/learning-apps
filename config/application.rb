c2393f39a361b192ee96303ee7535b09d9c42e96
saravananNV <saranjasminenv@gmail.com>
2013-10-07 13:07:11 +0530

    modified newrelic

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
 require 'will_paginate/array'
+
+
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
